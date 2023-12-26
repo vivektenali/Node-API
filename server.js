858a8ff00095205b75fa9604ddb190d1e6e7b6d6
vivektenali <vivektenali@gmail.com>
2023-12-26 09:28:06 +0530

    Implemented a Custom Middleware to handle an Error

--- server.js
+++ server.js
@@ -2,12 +2,19 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const productRoute = require("./routes/productRoute");
+const errMiddleware = require("./middleware/errorMiddleware");
 const app = express();
 
 const MONGO = process.env.MONGO_URL;
 const PORT = process.env.PORT;
 
 app.use("/api/products", productRoute);
+
+app.get("/", (req, res) => {
+  throw new Error("fake");
+});
+
+app.use(errMiddleware);
 
 app.use(express.json());
 mongoose
